29183295bd05c7fa51ad91ede36b168774e20e9c
quang159258 <quang159258@gmail.com>
2023-11-08 23:42:38 +0700

    Update SFML

--- Banker/Main.cpp
+++ Banker/Main.cpp
@@ -1,31 +1,32 @@
-﻿#include"SFML\Graphics.hpp"
+﻿#include<iostream>
+#include<vector>
+#include<conio.h>
+#include<string>
+#include<thread>
+#include"mywindow.h"
+#include"Banker.h"
 
+#define v std::vector
+#define pause system("pause")
+#define cls system("cls")
 int main()
 {
-    // create the window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
-
-    // run the program as long as the window is open
-    while (window.isOpen())
-    {
-        // check all the window's events that were triggered since the last iteration of the loop
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            // "close requested" event: we close the window
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        // clear the window with black color
-        window.clear(sf::Color::Black);
-
-        // draw everything here...
-        // window.draw(...);
-
-        // end the current frame
-        window.display();
-    }
-
-    return 0;
+	v<int> Avail = { 3,3,2 };
+	v<v<int>> Allocation = { {  0,1, 0},
+							{ 2,0, 0},
+							{  3,0, 2 },
+							{ 2,1,1 },
+							{ 0, 0, 2 } };
+	v<v<int>> Max;
+	v<v<int>> Request;
+	v<v<int>> Need = {
+		{7,4,3},
+		{1,2,2},
+		{6,0,0},
+		{0,1,1},
+		{4,3,1} };
+	Banker m = Banker(5, 3, Avail, Allocation, Max, Request, Need);
+	m.AddRequest();
+	pause;
+	return 0;
 }
